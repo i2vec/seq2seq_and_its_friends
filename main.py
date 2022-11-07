c7608365f6ef409e70c8c92528512be405ca16e6
i2vec <xumingjun1024@126.com>
2022-11-07 14:30:13 +0800

    +attention

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from data_proc import ML_Dataset
-from models import Encoder, Decoder, Seq2Seq
+from models_attention import Encoder, Decoder, Seq2Seq, Attention
 import torch.optim as optim
 
 
@@ -12,12 +12,14 @@ ENC_OUTPUT_DIM = train_dataset.CN_data.n_words
 DEC_OUTPUT_DIM = train_dataset.EN_data.n_words
 DEC_HID_DIM = ENC_HID_DIM = 128
 DEC_EMB_DIM = ENC_EMB_DIM = 128
+EPOCHS = 30
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 lr = 0.001
 
 encoder = Encoder(ENC_OUTPUT_DIM, ENC_EMB_DIM, ENC_HID_DIM)
-decoder = Decoder(DEC_OUTPUT_DIM, DEC_EMB_DIM, DEC_HID_DIM)
-seq2seq = Seq2Seq(encoder=encoder, decoder=decoder)
+attention = Attention(ENC_EMB_DIM, DEC_HID_DIM)
+decoder = Decoder(DEC_OUTPUT_DIM, DEC_EMB_DIM, DEC_HID_DIM, attention)
+seq2seq = Seq2Seq(encoder, decoder, device)
 
 seq2seq.to(device)
 
@@ -26,12 +28,19 @@ dataloader = DataLoader(train_dataset, shuffle=True, batch_size=1)
 optimizer = optim.Adam(seq2seq.parameters(), lr=lr)
 criterion = nn.CrossEntropyLoss()
 
-for idx, (ch_sent, en_sent) in enumerate(dataloader):
-    outputs = seq2seq(ch_sent, en_sent)
-    outputs = outputs.view(-1, decoder.output_dim)
-    en_sent = en_sent.view(-1)
-    loss = criterion(outputs, en_sent)
-    loss.backward()
-    optimizer.zero_grad()
-    optimizer.step()
+print(seq2seq.state_dict)
+seq2seq.train()
+for epoch in range(EPOCHS):
+    sum = len(train_dataset)
+    loss_sum = 0
+    for idx, (ch_sent, en_sent) in enumerate(dataloader):
+        optimizer.zero_grad()
+        outputs = seq2seq(ch_sent, en_sent)
+        outputs = outputs.view(-1, decoder.output_dim)
+        en_sent = en_sent.view(-1)
+        loss = criterion(outputs, en_sent)
+        loss_sum += loss.detach().cpu()
+        loss.backward()
+        optimizer.step()
+    print(f'epoch: {epoch}|loss: {loss_sum / sum}')
     
--- models_attention.py
+++ models_attention.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 
@@ -16,24 +17,29 @@ class Encoder(nn.Module):
         words_input, words_input.unsqueeze(0)
         embeded = self.embedding(words_input)
         embeded = embeded.permute(1, 0, 2)
-        _, hid = self.rnn(embeded)
-        return hid
+        output, hid = self.rnn(embeded)
+        return output, hid
 
 
 class Decoder(nn.Module):
-    def __init__(self, output_dim, emb_dim, hid_dim) -> None:
+    def __init__(self, output_dim, emb_dim, hid_dim, attention) -> None:
         super().__init__()
+        self.attention = attention
         self.output_dim = output_dim
         self.emb_dim = emb_dim
         self.hid_dim = hid_dim
         self.embedding = nn.Embedding(output_dim, emb_dim)
-        self.rnn = nn.RNN(emb_dim, hid_dim)
+        self.rnn = nn.RNN(emb_dim+hid_dim, hid_dim)
         self.linear = nn.Linear(hid_dim, output_dim)
     
-    def forward(self, word_input: torch.LongTensor, hid):
+    def forward(self, word_input: torch.LongTensor, hid, encoder_output):
+        encoder_output = encoder_output.permute(1, 0, 2)
+        values = self.attention(encoder_output, hid)
+        values = values.unsqueeze(1)
+        weights = torch.bmm(values, encoder_output).permute(1, 0, 2)
         word_input = word_input.unsqueeze(0)
         embeded =  self.embedding(word_input)
-        embeded = embeded.permute(1, 0, 2)
+        embeded = torch.cat((embeded, weights), dim=-1).permute(1, 0, 2)
         output, hid =  self.rnn(embeded, hid)
         output = self.linear(output)
         return output, hid
@@ -51,12 +57,26 @@ class Seq2Seq(nn.Module):
         batch_size = trg.shape[0]
         trg_len = trg.shape[1]
         outputs = torch.zeros(trg_len, batch_size, self.decoder.output_dim).to(self.device)
-        hid = self.encoder(src)
+        encoder_output, hid = self.encoder(src)
         input = trg[:, 0]
         for i in range(1, trg.shape[1]):
-            output, hid = self.decoder(input, hid)
+            output, hid = self.decoder(input, hid, encoder_output)
             outputs[i] = output
             input = trg[:, i]   
         return outputs
+    
+class Attention(nn.Module):
+    def __init__(self, enc_emb_dim, dec_hid_dim) -> None:
+        super().__init__()
+        self.attn = nn.Linear(enc_emb_dim+dec_hid_dim, dec_hid_dim)
+        self.v = nn.Linear(dec_hid_dim, 1, bias=False)
+        
+    def forward(self, output, hid):
+        hid = hid.permute(1, 0, 2)
+        hid = hid.repeat(1, output.shape[1], 1)
+        kq =  torch.tanh(self.attn(torch.cat((hid, output), dim=-1)))
+        attention = self.v(kq).squeeze(-1)
+        return F.softmax(attention, dim=-1)
+
         
         
